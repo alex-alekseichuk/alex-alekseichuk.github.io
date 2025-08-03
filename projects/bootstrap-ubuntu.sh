bca2f0157db80a228119c616a2851d4ada469a09
Alex Alekseichuk <alex.alekseichuk@yandex.com>
2025-08-03 09:59:10 +0300

    bootstrap script

---
+++ projects/bootstrap-ubuntu.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+cd ~
+apt update -y
+DEBIAN_FRONTEND=noninteractive apt install -y \
+    wget curl git tmux zoxide neovim vifm zsh vimdiff ripgrep
+git clone https://github.com/alex-alekseichuk/.dotfiles.git
+mkdir -p ~/bin
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+mkdir -p ~/.config/vifm
+ln -s ~/.dotfiles/.config/vifm/vifmrc ~/.config/vifm/vifmrc
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
+
+for f in ~/.dotfiles/bin/*; do ln -s $f ~/bin/$(basename $f); done
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
