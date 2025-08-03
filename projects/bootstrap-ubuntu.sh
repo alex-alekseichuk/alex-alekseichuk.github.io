f9090d21946fc8c2bea737765ff5b6cc2b7c54db
Alex Alekseichuk <alex.alekseichuk@yandex.com>
2025-08-03 10:46:26 +0300

    split bootstrap script into bootstrap and install

--- projects/bootstrap-ubuntu.sh
+++ projects/bootstrap-ubuntu.sh
@@ -15,6 +15,5 @@ ln -s ~/.dotfiles/.zshrc ~/.zshrc
 for f in ~/.dotfiles/bin/*; do ln -s $f ~/bin/$(basename $f); done
 
 RUNZSH=yes CHSH=yes KEEP_ZSHRC=yes \
-    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" \
-    </dev/null
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
